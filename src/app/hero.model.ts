e05dba9918aa1c1a708615753c98852edac8a9f9
heltutan <heltutan@kpmg.com>
2022-12-08 22:29:11 +0100

    Improve Hero Model

---
+++ src/app/hero.model.ts
@@ -0,0 +1,11 @@
+export class Hero {
+  id: number;
+  name: string;
+  strength: number;
+
+  constructor(id: number, name: string, strength: number) {
+    this.id = id
+    this.name = name
+    this.strength = strength
+  }
+}
